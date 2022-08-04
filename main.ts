d4b21c507dec4d6673ea4fa39b315b7b1522a5c9
Alex-Web940 <110633160+Alex-Web940@users.noreply.github.com>
2022-08-04 16:03:50 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    Cabeza_dinosaurio.change(LedSpriteProperty.Y, -1)
+    Pies_dinosaurio.change(LedSpriteProperty.Y, -1)
+    basic.pause(900)
+    Cabeza_dinosaurio.change(LedSpriteProperty.Y, 1)
+    Pies_dinosaurio.change(LedSpriteProperty.Y, 2)
+})
+function Piedrita () {
+    Piedra = game.createSprite(4, 4)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(500)
+        Piedra.change(LedSpriteProperty.X, -1)
+        if (Piedra.isTouching(Pies_dinosaurio)) {
+            basic.showIcon(IconNames.Angry)
+            music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+            Piedrita()
+        }
+    }
+}
+let Piedra: game.LedSprite = null
+let Cabeza_dinosaurio: game.LedSprite = null
+let Pies_dinosaurio: game.LedSprite = null
+Pies_dinosaurio = game.createSprite(0, 4)
+Cabeza_dinosaurio = game.createSprite(0, 3)
+Piedrita()
+basic.forever(function () {
+    if (true) {
+        Piedra.ifOnEdgeBounce()
+        basic.pause(100)
+        Piedra.delete()
+        Piedrita()
+    }
+})
