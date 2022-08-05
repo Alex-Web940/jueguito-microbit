2280f135559d933c1066ff79454b0f464a93fa30
Alex-Web940 <110633160+Alex-Web940@users.noreply.github.com>
2022-08-04 20:44:13 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,27 @@
 input.onButtonPressed(Button.A, function () {
-    Cabeza_dinosaurio.change(LedSpriteProperty.Y, -2)
-    Pies_dinosaurio.change(LedSpriteProperty.Y, -1)
+    Cabeza_dinosaurio.change(LedSpriteProperty.Y, -1)
+    Pies_dinosaurio.change(LedSpriteProperty.Y, -2)
     basic.pause(900)
-    Cabeza_dinosaurio.change(LedSpriteProperty.Y, 3)
-    Pies_dinosaurio.change(LedSpriteProperty.Y, 1)
+    Cabeza_dinosaurio.change(LedSpriteProperty.Y, 1)
+    Pies_dinosaurio.change(LedSpriteProperty.Y, 2)
 })
 function Piedrita () {
     Piedra = game.createSprite(4, 4)
     for (let index = 0; index < 4; index++) {
         basic.pause(500)
         Piedra.change(LedSpriteProperty.X, -1)
-        if (Piedra.isTouching(Pies_dinosaurio)) {
-            basic.showIcon(IconNames.Angry)
+        if (Piedra.isTouching(Cabeza_dinosaurio)) {
             music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+            basic.showIcon(IconNames.Angry)
             Piedrita()
         }
     }
 }
 let Piedra: game.LedSprite = null
-let Cabeza_dinosaurio: game.LedSprite = null
 let Pies_dinosaurio: game.LedSprite = null
-Pies_dinosaurio = game.createSprite(0, 4)
+let Cabeza_dinosaurio: game.LedSprite = null
 Cabeza_dinosaurio = game.createSprite(0, 3)
+Pies_dinosaurio = game.createSprite(0, 4)
 Piedrita()
 basic.forever(function () {
     if (true) {
